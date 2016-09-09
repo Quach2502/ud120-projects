8ed8a06d654c660caf28cc0637c2e41d5ab5da21
Quach <quachdung2502@gmail.com>
2016-09-09 11:23:13 +0800

    solved

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,38 @@
     Sara has label 0
     Chris has label 1
 """
-    
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+# create classifier
+clf = GaussianNB()
+# fit/train
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+# predict
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+# accuracy
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
+
 
 
 
 
 #########################################################
-### your code goes here ###
+# your code goes here ###
 
 
 #########################################################
-
-
