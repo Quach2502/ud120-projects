857ad8231e8f3ff8e3757fc5e92e71de182a5928
Quach <quachdung2502@gmail.com>
2016-09-09 14:50:54 +0800

    solved

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,24 +7,31 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+# classifer
+clf = SVC(kernel='rbf', C=10000)
+# fit
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time() - t0, 3), "s"
+# predict
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time: ", round(time() - t0, 3), "s"
+# accuracy
+accuracy = accuracy_score(labels_test, pred)
+# how many email belongs to class (1)
+result = [n for n in pred if n == 1]
+print len(result)
